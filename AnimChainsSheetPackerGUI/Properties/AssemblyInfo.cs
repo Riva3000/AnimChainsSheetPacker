18cce2789d36cbca7b2c25c3817b511d304c0019
Riva3000 <microriva@gmail.com>
2017-05-22 13:15:10 +0200

    Minor projects settings fixes. (Really minor. 10 years old at most)

--- AnimChainsSheetPackerGUI/Properties/AssemblyInfo.cs
+++ AnimChainsSheetPackerGUI/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TexturePackerImport")]
+[assembly: AssemblyProduct("AnimChainsSheetPackerGUI")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
