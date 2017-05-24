5e4f3c876a0992521b5279e40f20e8258eb1a354
Riva3000 <microriva@gmail.com>
2017-05-24 07:19:17 +0200

    Removed unused references

--- AnimChainsSheetPacker/Testing.cs
+++ AnimChainsSheetPacker/Testing.cs
@@ -1,15 +1,4 @@
-﻿using FlatRedBall.Graphics.Animation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-using System.Web.Script.Serialization; // JavaScriptSerializer
-using FlatRedBall.Content.AnimationChain;
-
-using AnimChainsSheetPacker.DataTypes;
+﻿using FlatRedBall.Content.AnimationChain;
 
 
 namespace AnimChainsSheetPacker
