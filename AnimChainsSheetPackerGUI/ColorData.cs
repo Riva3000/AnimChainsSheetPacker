59678263d07e48a5ad6a2085d03eb8525bd8ccde
Riva3000 <microriva@gmail.com>
2017-07-26 22:59:41 +0200

    Squash Merge of branch [GuiAppWork] at Commit 1e9191d1
    ---Contains following Commits:
    
    commit 0583cea72c297b3304a379e6329a888f5b788eef
    Author: Riva3000 <microriva@gmail.com>
    Date:   Wed Jul 26 22:52:54 2017 +0200
    
        Removed debug paths.
    
    commit 1e9191d14008ea51f1768901a9014d4d54764fff
    Author: Riva3000 <microriva@gmail.com>
    Date:   Wed Jul 26 22:31:35 2017 +0200
    
        GUI:
          Bugfixes: Wrong directory select dialogues
          Features added: result sheet Transparent color change
    
    commit 3919773634f8595922e7460f6122552769731e25
    Author: Riva3000 <microriva@gmail.com>
    Date:   Wed Jul 26 22:25:45 2017 +0200
    
        Fixed bug: not detecting all duplicate frames
    
    commit cf721851ea16efcebe58f21a132d9a9de59980e2
    Author: Riva3000 <microriva@gmail.com>
    Date:   Fri May 26 04:23:21 2017 +0200
    
        Adding KnownColors v2 - Color.Name problem
    
    commit 9abec7f6284a22e9dc016aaad7c73a88ff081881
    Author: Riva3000 <microriva@gmail.com>
    Date:   Fri May 26 04:02:43 2017 +0200
    
        x
    
    commit 7f54370d5c1ab18b49ac6ad8d28cdc6ef750091a
    Author: Riva3000 <microriva@gmail.com>
    Date:   Fri May 26 03:35:55 2017 +0200
    
        Added KnownColors from StackOverflow example

---
+++ AnimChainsSheetPackerGUI/ColorData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimChainsSheetPacker
+{
+    public struct ColorData
+    {
+        public readonly Color Color;
+        private readonly string _Name;
+        public string Name
+        {
+            get
+            {
+                if (_Name != null)
+                    return _Name;
+
+                return Color.Name;
+            }
+        }
+
+        public ColorData(Color color, string name)
+        {
+            Color = color;
+            _Name = name;
+        }
+    }
+}
